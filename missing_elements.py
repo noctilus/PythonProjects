719d243f03a4705fc09ca99783530f65fe44b931
noctilus <noctilus@gmail.com>
2018-09-26 20:39:43 +0200

    reg update

--- missing_elements.py
+++ missing_elements.py
@@ -7,3 +7,12 @@
 # wyjście: [1,5,6,8,10]
 
 
+entry_elements = [2, 3, 7, 4, 9]
+full_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  # for larger lists i+=1 and full_list.append() ?
+
+
+def missing_elements():
+    for x in entry_elements:
+        full_list.remove(x)
+
+# missing_elements()

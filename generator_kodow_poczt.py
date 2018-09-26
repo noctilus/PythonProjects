7d1654f35e753733b86bd9fd109c71b0fc250ebc
noctilus <noctilus@gmail.com>
2018-09-26 19:33:28 +0200

    reg update

--- generator_kodow_poczt.py
+++ generator_kodow_poczt.py
@@ -6,13 +6,13 @@ lower_limit = "79-900"
 upper_limit = "79-903"
 
 
-def generator(lower_limit, upper_limit):
+def generator():
     lower_int = int(lower_limit.replace("-", ""))
     upper_int = int(upper_limit.replace("-", ""))
 
     for i in range(lower_int, upper_int - 1):
         i += 1
-        print(i)
+        print(str(i / 1000).replace(".", "-"))
 
 
-generator("79-900", "79-905")
+generator()

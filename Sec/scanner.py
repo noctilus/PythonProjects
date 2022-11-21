163d5e67d594e89048abe5f8e94b461f73077864
Marcin Łańcucki <noctilus@gmail.com>
2022-11-21 15:44:58 +0100

    .

---
+++ Sec/scanner.py
@@ -0,0 +1,20 @@
+"""
+pinging python script
+"""
+import socket
+import sys
+from datetime import datetime
+
+if len(sys.argv) == 2:
+    target = socket.gethostname()
+
+else:
+    print("Invalid number of arguments")
+    print("Usage: python3 scanner.py <ip>")
+    sys.exit()
+
+# Add pretty banner
+print("-"*25)
+print("Scanning target "+target)
+print("Time started: " + str(datetime.now()))
+print("-"*25)

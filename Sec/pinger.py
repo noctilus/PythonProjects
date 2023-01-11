7eabd57dcde653f6c3130049972e5b6dea7c416f
Marcin Łańcucki <noctilus@gmail.com>
2023-01-11 08:54:42 +0100

    '

--- Sec/pinger.py
+++ Sec/pinger.py
@@ -1,17 +1,50 @@
-import socket
-import sys
-from datetime import datetime
+# import sys
+import datetime
+import os
+import platform
 
-if len(sys.argv) == 2:
-    target = socket.gethostname()
+print("""
+      1. Enter an IP range 
+      2. first number to ping
+      3. last number to ping
 
+      Example:
+      -192.168.0
+      -2
+      -8
+
+      """)
+
+user_input = input("Enter network IP: ")
+ip_parts = user_input.split(".")
+network_ip = ip_parts[0]+'.'+ip_parts[1]+'.'+ip_parts[2]+'.'
+
+first_host = int(input("Enter first number: "))
+last_host = int(input("Enter last number: "))
+last_host += 1
+
+oper = platform.system()
+
+if (oper == "Windows"):
+    ping = "ping -n 1 "
 else:
-    print("Invalid number of arguments")
-    print("Usage: python3 scanner.py <ip>")
-    sys.exit()
+    ping = "ping -c 1 "
 
-# Add pretty banner
-print("-"*25)
-print("Scanning target "+target)
-print("Time started: " + str(datetime.now()))
-print("-"*25)
+time1 = datetime.datetime.now()
+print("Scanning in progress")
+
+for ip in range(first_host, last_host):
+    addr = network_ip + str(ip)
+    command = ping + addr
+    response = os.popen(command)
+    print(ip)
+    list = response.readlines()
+
+    for line in list:
+        if (line.count("ttl=")):
+            print(addr, "--> live")
+            break
+
+time2 = datetime.datetime.now()
+total = time2 - time1
+print("Scan completed in ", total)

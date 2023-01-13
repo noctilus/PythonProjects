21952fd7ae67f983203c7760d8f5018dd8751b62
Marcin Łańcucki <noctilus@gmail.com>
2023-01-13 21:45:01 +0100

    .

---
+++ open_ai.py
@@ -0,0 +1,9 @@
+import openai
+
+openai.Completion.create(
+    engine="davinci",
+    prompt="Make a list of astronomical observatories:"
+)
+
+API_KEY = "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"
+ORGANIZATION_ID = "org-C2kvhijI4C3q2iOFpfdQxan9"

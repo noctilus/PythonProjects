e320be9803cf5ed0c89e971d2373d57a9fe24afc
Marcin Łańcucki <noctilus@gmail.com>
2023-01-15 12:18:27 +0100

    .

--- open_ai.py
+++ open_ai.py
@@ -1,9 +1,29 @@
-import openai
+# import openai_secret_manager
+# import openai
+import requests
 
-openai.Completion.create(
-    engine="davinci",
-    prompt="Make a list of astronomical observatories:"
+API_KEY = "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"
+ORGANIZATION_ID = "org-C2kvhijI4C3q2iOFpfdQxan9"
+
+# Get API key
+# secrets = openai.get_secrets("openai")
+# api_key = secrets["api_key"]
+
+# Ask user for input
+question = input("What is your question? ")
+
+# Send request to API
+response = requests.post(
+    "https://api.openai.com/v1/engines/davinci-codex/completions",
+    headers={"Content-Type": "application/json",
+
+             "Authorization": f"Bearer {API_KEY}"},
+    json={
+        "prompt": question,
+        "temperature": 0.5,
+    },
 )
 
-API_KEY = "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"
-ORGANIZATION_ID = "org-C2kvhijI4C3q2iOFpfdQxan9"
+# Print response
+z = response.json()
+print(response.json()["choices"][0]["text"])

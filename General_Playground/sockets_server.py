0a525ecfb773effec7c0a5078306b5e39af4ffa7
noctilus <noctilus@gmail.com>
2024-05-31 15:41:16 +0200

    server python script writes to file

--- General_Playground/sockets_server.py
+++ General_Playground/sockets_server.py
@@ -1,17 +1,24 @@
 import socket
 
-HOST = "127.0.0.1"
+HOST = "0.0.0.0"
 PORT = 9000
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+while True:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
-
     with conn:
         print(f"Connected from {addr}")
         while True:
             data = conn.recv(1024)
             if not data:
                 break
+            log_file = open("temp.logs", "a", encoding="utf-8")
+            print("Data: ", data.decode("utf-8"))
+            data_to_write = f"{data.decode()}\n"
+            log_file.write(data_to_write)
+            log_file.close()
             conn.sendall(data)
+            s.close()

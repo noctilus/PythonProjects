da9d4300f43aaaf8bb9f02f4fac03ac41c8bfa6c
noctilus <noctilus@gmail.com>
2024-06-07 07:30:41 +0200

    .

---
+++ General_Playground/Log_Analysis/offending_ips.py
@@ -0,0 +1,26 @@
+names: set[str] = set()
+ips: set[str] = set()
+all_names = list()
+all_ips = list()
+
+with open("./invalid_users.txt") as f:
+    lines = f.readlines()
+    for line in lines:
+        username = ((line.split(" ")))[7]
+        ip_number = ((line.split(" ")))[9]
+        # names.add(str(username))
+        # ips.add(str(ip_number))
+        all_names.append(username)
+        all_ips.append(ip_number)
+koncowy_plik_ips: dict = dict()
+for ip in all_ips:
+    if ip not in koncowy_plik_ips:
+        koncowy_plik_ips[ip] = int(1)
+    else:
+        x: int = koncowy_plik_ips.get(ip) + 1
+        koncowy_plik_ips.update({ip: x})
+koncowa_lista = dict(
+    sorted(koncowy_plik_ips.items(), key=lambda item: item[1], reverse=True)
+)
+for i, k in koncowa_lista.items():
+    print(i, k)

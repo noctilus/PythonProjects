2510001bea39813f90c147a5e01a6447508df49d
noctilus <noctilus@gmail.com>
2024-06-13 18:00:46 +0200

    .

--- Sockets_Server/server_sockets.py
+++ Sockets_Server/server_sockets.py
@@ -1,3 +1,4 @@
+import json
 import socket
 import sys
 
@@ -6,6 +7,7 @@ PORT = 9000
 
 while True:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Don't block the socket after creating it
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST, PORT))
     s.listen()
@@ -16,12 +18,19 @@ while True:
             data = conn.recv(1024)
             if not data:
                 break
-            try:  # data_to_write = data.decode(encoding="utf-8")
+            try:
+                # Can't use with open(): because there's a problem with
+                # Error handling
                 log_file = open("temp.logs", "a", encoding="utf-8")
+                # decode received data from bytes to string
                 decoded_data = data.decode(encoding="utf-8")
-                print(decoded_data)
+                print(type(decoded_data), decoded_data)
+                # decode string to json object
+                decoded_data = json.loads(decoded_data)
+                # format the json object to write to file and write
                 data_to_write = f"({decoded_data}\n)"
                 log_file.write(data_to_write)
+                log_file.close()
                 conn.sendall(data)
                 s.close()
             except UnicodeError as e:
--- Sockets_Server/sockets_client.py
+++ Sockets_Server/sockets_client.py
@@ -1,11 +1,35 @@
+import datetime
+import json
+import random
 import socket
 
 HOST = "141.95.127.166"
 PORT = 9000
 
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    temp = random.randrange(1, 30)
+    press = random.randrange(890, 1024)
+    MACHINE_ID = "083a72ac7060"
+    timestamp = str(datetime.datetime.now())
+    # Formulating dict{} from data provided by sensors and device
+    data_to_send = {
+        "press": press,
+        "temp": temp,
+        "machine_id": MACHINE_ID,
+        "timestamp": timestamp,
+    }
+    # serializing data from dict{} to json object
+    data_to_send_json = json.dumps(data_to_send)
+    # json object (string) needs to be encoded to bytes
+    # in order to send through the socket
+    data_to_send_encoded = data_to_send_json.encode(encoding="utf-8")
+    # connecting to data server
     s.connect((HOST, PORT))
-    s.sendall(b"Helo World!")
+    # sending data
+    # sendall() closes socket after sending is finished afaik
+    s.sendall(data_to_send_encoded)
+    # reading the data sent back by data server
     data = s.recv(1024)
 
 print(f"received,{data!r}")
